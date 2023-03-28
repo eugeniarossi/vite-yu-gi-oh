8e4e3191a8f847b770510895d9fd22217f4cbd6e
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-28 21:32:22 +0200

    feat: logo

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -5,7 +5,14 @@ export default {
 </script>
 
 <template>
-    <h2 class="fs-3 py-3 px-5">Yu-Gi-Oh Api</h2>
+    <header class="d-flex align-items-center px-3 py-4">
+        <img src="../../public/logo.png" alt="logo">
+        <h2 class="fs-3 px-3">Yu-Gi-Oh Api</h2>
+    </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+img {
+    max-width: 4.0625rem;
+}
+</style>
