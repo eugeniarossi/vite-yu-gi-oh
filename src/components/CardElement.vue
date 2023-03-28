689e33dc0df2b7206a795fb9b96aae46b58b737a
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-28 09:34:11 +0200

    feat: card api

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -1,15 +1,20 @@
 <script>
 export default {
-    name: 'Card'
+    name: 'Card',
+    props: {
+        img: String,
+        title: String,
+        type: String
+    }
 }
 </script>
 
 <template>
     <div class="card rounded-0 border-0 text-center" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img :src="img" class="card-img-top" :alt="name">
         <div class="card-body">
-            <h6 class="card-title">Card title</h6>
-            <span>alien</span>
+            <h6 class="card-title">{{ name }}</h6>
+            <span>{{ type }}</span>
         </div>
     </div>
 </template>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,10 +1,24 @@
 <script>
+import axios from 'axios';
 import CardElement from './CardElement.vue';
 
 export default {
     name: 'CardsList',
     components: {
         CardElement
+    },
+    data() {
+        return {
+            cardsData: []
+        }
+    },
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then((response) => {
+            console.log(response);
+            this.cardsData = response.data.data;
+            console.log(this.cardsData)
+        })
     }
 }
 </script>
@@ -12,8 +26,10 @@ export default {
 <template>
     <div class="container p-0">
         <div class="row g-3">
-            <div class="col-12 col-sm-3">
-                <CardElement></CardElement>
+            <div class="col-12 col-sm-3" v-for="card in cardsData.slice(0,50)">
+                <CardElement :img="card.card_images[0].image_url"
+                :title="card.name"
+                :type="card.archetype"/>
             </div>
         </div>
     </div>
